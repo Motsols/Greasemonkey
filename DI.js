6057ec59fa7815f66c01433435b5deca18279943
Motsols <derrick.eriksson@gmail.com>
2017-06-27 11:17:46 +0200

    tha files motha fucka

---
+++ DI.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         DI Fix
+// @namespace    DagensIndustri
+// @version      0.1
+// @description  Fixes DI's shitty website
+// @author       TalCet
+// @match        http://www.di.se/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    var removeGiantHeader = function() {
+        var giantHeader = document.querySelector('.di_panorama-wrapper');
+        giantHeader.style.display='none';
+    };
+    var removeSecondGiantHeader = function() {
+        var header = document.querySelector('.di_header');
+        header.style.display='none';
+    };
+    var removeStickyHeader = function(){
+        var header = document.querySelector('.di_header__sticky-wrapper');
+        header.style.display='none';
+    };
+    var centerPage = function(){
+        var div = document.querySelector('.di_main-area');
+        div.style.position = 'relative';
+        div.style.marginLeft = 'auto';
+        div.style.marginRight = 'auto';
+    };
+
+    removeGiantHeader();
+    removeSecondGiantHeader();
+    removeStickyHeader();
+    centerPage();
+
+    var myInterval = window.setInterval(function() {
+
+        var paywallCont = document.querySelector('.paywall-content');
+        var payMe = document.querySelector('#serviceplusPaywallpaywall-container');
+
+        if(paywallCont){
+            paywallCont.style.height = 'auto';
+            payMe.style.display = 'none';
+
+        }
+
+        if (paywallCont && paywallCont.style.height === 'auto')
+            clearInterval(myInterval);
+
+    }, 1000);
+
+})();
---
+++ DN.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         DN-script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Enhances the user experience (hehe) of dn.se
+// @author       TalCet
+// @match        http://www.dn.se/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    var removeArticleBanner = function() {
+        var adPanorama = document.querySelector('.ad.ad--panorama');
+        var article = document.querySelector('.js-article');
+        article.removeChild(adPanorama);
+    };
+
+    var removePaywall = function() {
+        var premContent = document.querySelector('.article__premium-content');
+        var payWallOverlay = document.querySelector('.js-paywall.paywall');
+
+        premContent.style.display = 'block';
+        payWallOverlay.style.display = 'none';
+    };
+
+    var removeAdBlockedOverlay = function(){
+        var poopface = document.querySelector('#pwOverlay');
+
+        poopface.style.display = 'none';
+    };
+
+    removeArticleBanner();
+    removePaywall();
+    removeAdBlockedOverlay();
+
+})();
